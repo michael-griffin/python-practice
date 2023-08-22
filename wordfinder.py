e3f9d6976e05c4773aa8d2690b3e3f25ed6c7e4e
Jung-GunSong <jsong781@gmail.com>
2023-08-22 18:35:15 -0400

    working on SpecialWordFinder subclass

--- wordfinder.py
+++ wordfinder.py
@@ -1,24 +1,24 @@
+from random import randint
+
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
 
-    #__init__ should take a file path
-        #file should have 1 word per line
-        #one attribute should be list of these words
     def __init__(self, path):
         self.path = path
         self.word_list = self.get_words()
         self.print_num()
 
-    #get words doesn't add to word_list/return a word list.
     def get_words(self):
-        #should read through self.path, then return a list of words
-        #"words.txt"
 
-        #initialize empty list, word_list
+        line_list = []
         with open(self.path, "r") as file:
             for line in file:
 
-        #should return a list
-    #done
+                line_list.append(line.strip())
+        return line_list
+
     def print_num(self):
         print(f"{len(self.word_list)} words read")
+
+    def random(self):
+        return self.word_list[randint(0,len(self.word_list)-1)]
