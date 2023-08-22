6a0de38d987395c71235d49330ebfb00f8be6348
Michael <michaellngriffin@gmail.com>
2023-08-22 03:45:08 -0400

    completed serial, began work on wordfinder

--- serial.py
+++ serial.py
@@ -1,6 +1,6 @@
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -17,3 +17,24 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+
+    #TODO: write __repr__
+    def __repr__(self):
+        return f"<serial current={self.current}, original={self.original}>"
+
+    """initialize serial with a start number, then set current and original to start"""
+    def __init__(self, start=100):
+        self.current = start
+        self.original = start
+
+    """returns the current value of serial instance, then increments it"""
+    def generate(self):
+        curr_value = self.current
+        self.current += 1
+        return curr_value
+
+    """resets back to the initially passed in value, and then returns it"""
+    def reset(self):
+        self.current = self.original
+        #return self.original
+
--- wordfinder.py
+++ wordfinder.py
@@ -1,2 +1,24 @@
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
+
+    #__init__ should take a file path
+        #file should have 1 word per line
+        #one attribute should be list of these words
+    def __init__(self, path):
+        self.path = path
+        self.word_list = self.get_words()
+        self.print_num()
+
+    #get words doesn't add to word_list/return a word list.
+    def get_words(self):
+        #should read through self.path, then return a list of words
+        #"words.txt"
+
+        #initialize empty list, word_list
+        with open(self.path, "r") as file:
+            for line in file:
+
+        #should return a list
+    #done
+    def print_num(self):
+        print(f"{len(self.word_list)} words read")
